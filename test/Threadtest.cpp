10f514b54a0c490f78950017028ab9de3d81d0d6
Saber Alonso <li_bei_happy@126.com>
2014-02-26 23:36:28 +0800

    socket C++
    Socket Communication C++

---
+++ test/Threadtest.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+using namespace std;
+HANDLE hMutex;
+HANDLE hThead;
+DWORD WINAPI Fun(LPVOID lpParamter)
+{
+	while(1) { 
+		WaitForSingleObject(hMutex, INFINITE);
+		cout<<"Fun display!"<<endl; 
+		Sleep(1000);
+		ReleaseMutex(hMutex);
+}
+
+int main()
+{
+	HANDLE hThread = CreateThread(NULL, 0, Fun, NULL, 0, NULL);
+	hMutex = CreateMutex(NULL, FALSE, "screen");
+	CloseHandle(hThread);
+	while(1) {
+		WaitForSingleObject(hMutex, INFINITE);
+		cout<<"main display!"<<endl;  
+		Sleep(2000);
+		ReleaseMutex(hMutex);
+	}
+	return 0;
+}
---
+++ test/client.cpp
@@ -0,0 +1,48 @@
+#include "StdAfx.h"
+#include <stdio.h>
+#pragma comment(lib,"ws2_32.lib")
+#include <Winsock2.h>
+void main()
+{
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+
+	wVersionRequested = MAKEWORD( 1, 1 );
+
+	err = WSAStartup( wVersionRequested, &wsaData );
+	if ( err != 0 ) {
+		return;
+	}
+
+	if ( LOBYTE( wsaData.wVersion ) != 1 ||
+		HIBYTE( wsaData.wVersion ) != 1 ) {
+			WSACleanup( );
+			return;
+	}
+	SOCKET sockSrv=socket(AF_INET,SOCK_STREAM,0);
+
+	SOCKADDR_IN addrSrv;
+	addrSrv.sin_addr.S_un.S_addr=htonl(INADDR_ANY);
+	addrSrv.sin_family=AF_INET;
+	addrSrv.sin_port=htons(6000);
+
+	bind(sockSrv,(SOCKADDR*)&addrSrv,sizeof(SOCKADDR));
+
+	listen(sockSrv,5);
+
+	SOCKADDR_IN addrClient;
+	int len=sizeof(SOCKADDR);
+	while(1)
+	{
+		SOCKET sockConn=accept(sockSrv,(SOCKADDR*)&addrClient,&len);
+		char sendBuf[50];
+		sprintf(sendBuf,"Welcome %s to here!",inet_ntoa(addrClient.sin_addr));
+		send(sockConn,sendBuf,strlen(sendBuf)+1,0);
+		char recvBuf[50];
+		recv(sockConn,recvBuf,50,0);
+		printf("%s\n",recvBuf);
+		closesocket(sockConn);
+	}
+
+}
